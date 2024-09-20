1180f43a485552a83f453bd3e9b7bd5e515d1e7f
Prabdeep Dhaliwal <prab.dhaliwal95@gmail.com>
2024-09-20 12:08:11 -0400

    Update

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -497,6 +497,53 @@ public static class Leetcode
     #endregion
 }
 
+//232. Implement Queue using Stacks
+public class MyQueue
+{
+    Stack<int> stack1 = new Stack<int>();
+    Stack<int> stack2 = new Stack<int>();
+
+    public MyQueue()
+    {
+        stack1 = new Stack<int>();
+        stack2 = new Stack<int>();
+    }
+
+    public void Push(int x)
+    {
+        stack1.Push(x);
+    }
+
+    public int Pop()
+    {
+        if (stack2.Count == 0)
+        {
+            while (stack1.Count > 0)
+            {
+                stack2.Push(stack1.Pop());
+            }
+        }
+        return stack2.Pop();
+    }
+
+    public int Peek()
+    {
+        if (stack2.Count == 0)
+        {
+            while (stack1.Count > 0)
+            {
+                stack2.Push(stack1.Pop());
+            }
+        }
+        return stack2.Peek();
+    }
+
+    public bool Empty()
+    {
+        return stack1.Count == 0 && stack2.Count == 0;
+    }
+}
+
 //Definition for a linked list node.
 public class ListNode
 {
